4f451faa494509c9fc9aedf408f5449b9c698459
David Triana <davidtriana796@gmail.com>
2023-08-18 12:10:37 -0500

    ejercicio 4

---
+++ ejercicio3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath> // Incluir la librería para funciones matemáticas
+
+using namespace std;
+
+int main() {
+   double hora, semana, mes;
+
+   hora = 5.000;
+
+   
+    // Mostrar resultados
+    cout << "El perimetro del circulo es: " << perimetro << endl;
+    cout << "El area del circulo es: " << area << endl;
+
+    return 0;
+}
