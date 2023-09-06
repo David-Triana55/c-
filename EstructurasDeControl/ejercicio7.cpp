a4bd0458817f0b44511b46b8e701f9f84c0ed908
David Triana <davidtriana796@gmail.com>
2023-09-06 17:06:40 -0500

    ejercicios finalizados

--- EstructurasDeControl/ejercicio7.cpp
+++ EstructurasDeControl/ejercicio7.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
-int main(){
-    // ! primos
+int main() {
+    int numero;
+    bool esPrimo = true;
+
+    cout << "Ingrese un numero entero positivo: ";
+    cin >> numero;
+
+    if (numero <= 1) {
+        esPrimo = false;  // Si el número es 0 o 1, no es primo
+    } else {
+        for (int i = 2; i * i <= numero; i++) {
+            if (numero % i == 0) {
+                esPrimo = false;  // Si encuentra un divisor, no es primo
+                break;  // Salir del bucle
+            }
+        }
+    }
+
+    if (esPrimo) {
+        cout << numero << " es un numero primo." << endl;
+    } else {
+        cout << numero << " no es un numero primo." << endl;
+    }
 
     return 0;
 }
